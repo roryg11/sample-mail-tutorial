6dcf844d7fb91436c978281aaf570a910b131516
roryg11 <Rory.C.Grant@gmail.com>
2016-05-22 20:39:28 -0700

    fixing syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :doman                => "gmail.com"
+   :doman                => "gmail.com",
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
    :authentication       => "plain",
